ab0b82dbb10bd23c902792de7ed6621cdb499077
amir.elahi.uxd <amir.elahi.uxd@gmail.com>
2024-05-28 04:57:06 +0330

    separate components and layout for sidebar

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,14 +1,15 @@
 import axios from "axios";
 export default defineNuxtPlugin((nuxtApp) => {
-  const defaultUrl = import.meta.env.NUXT_PUBLIC_API_BASE
+  const defaultUrl = "https://run.mocky.io";
 
   let api = axios.create({
     baseURL: defaultUrl,
     headers: {
+      "Content-Type": "application/json",
       common: {},
     },
   });
-return {
+  return {
     provide: {
       api: api,
     },
---
+++ stores/item.js
@@ -0,0 +1,9 @@
+// We don't need store according to requirement and scale of project
+
+export const useItemStore = defineStore("item", {
+  state: () => ({
+    items: [],
+  }),
+  getters: [],
+  actions: {},
+});
